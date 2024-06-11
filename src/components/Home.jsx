7266c3a702ef3398cc8a69f78a9735877a615e68
safabeegum <pasafabeegum@gmail.com>
2024-06-11 22:44:47 +0530

    Completed

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -9,7 +9,7 @@ const Home = () => {
             <div className="row">
                 <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
                 <div class="card">
-                <img src="https://img.freepik.com/free-photo/3d-rendering-classic-interior_23-2150943454.jpg?t=st=1718040777~exp=1718044377~hmac=b8a7eaffa04d4eef6ee9ff49d3dc02ab4172bb6f50d62240f210738fa38d4c2d&w=900" class="card-img-top" alt="..."></img>
+                <img src="https://img.freepik.com/free-photo/red-haired-lady-eyeglasses-holds-books-shows-ok-sign_197531-23789.jpg?w=1060&t=st=1718126041~exp=1718126641~hmac=031377469b358a9ebfd63aa2ceb196d61f4f462526e5ec6deb23179d5c821c59" class="card-img-top" alt="..."></img>
   
                 </div>
                 </div>
